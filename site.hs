fb9446a5ad238c041c407cb2e7e35a1f9aeab491
Alexander Lobov <alexanderlobov@yandex.ru>
2017-02-05 01:22:36 +0300

    Clean urls without html extension

--- site.hs
+++ site.hs
@@ -2,6 +2,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import System.FilePath.Posix ( takeDirectory
+                             , takeBaseName
+                             , (</>)
+                             , splitFileName
+                             )
+import Data.List (isInfixOf)
+import Control.Applicative (empty)
 
 
 config :: Configuration
@@ -21,14 +28,15 @@ main = hakyllWith config $ do
         compile compressCssCompiler
 
     match "posts/*" $ do
-        route $ setExtension "html"
+        route niceRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+            >>= removeIndexHtml
 
     create ["archive.html"] $ do
-        route idRoute
+        route niceRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
@@ -40,6 +48,7 @@ main = hakyllWith config $ do
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
 
     match "index.html" $ do
@@ -55,6 +64,7 @@ main = hakyllWith config $ do
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
     match "templates/*" $ compile templateCompiler
 
@@ -63,4 +73,30 @@ main = hakyllWith config $ do
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
+    myUrlField `mappend`
     defaultContext
+
+myUrlField :: Context a
+myUrlField = field "myurl" $
+    fmap (maybe empty $ removeIndexStr . toUrl) . getRoute . itemIdentifier
+
+
+niceRoute :: Routes
+niceRoute = customRoute createIndexRoute
+    where createIndexRoute identifier =
+            takeDirectory p </> takeBaseName p </> "index.html"
+            where p = toFilePath identifier
+
+removeIndexHtml :: Item String -> Compiler (Item String)
+removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
+
+removeIndexStr :: String -> String
+removeIndexStr url = case splitFileName url of
+    (dir, "index.html") | isLocal dir -> dir
+                        | otherwise -> url
+    _ -> url
+    where
+        isLocal :: String -> Bool
+        isLocal uri = not ("://" `isInfixOf` uri)
+
+
